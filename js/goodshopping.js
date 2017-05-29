5c31f5c10bbeda00164c78028720b85662881fb3
Jack <Jackzhang700@hotmail.com>
2017-05-28 23:56:39 -0700

    onclick

--- js/goodshopping.js
+++ js/goodshopping.js
@@ -5,13 +5,14 @@ var prev = document.getElementById('prev');
 var next = document.getElementById('next');*/
 var list = document.getElementById('list');
 function moveleft(){
-	list.style.left = parseInt(list.style.left) - 810 + 'px'
+	//list.style.left = parseInt(list.style.left) - 810 + 'px'
+	alert("panda!");
 	}
 /*prev.onclick = function(){
 	list.style.left = parseInt(list.style.left) + 600 + 'px'
 	}*/
 	
-	// alert("panda!");
+	// 
 
 
 
