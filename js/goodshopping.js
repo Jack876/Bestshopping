99cfd146acda552a76f95ed37eb11e2a6d7cb25a
Jack <Jackzhang700@hotmail.com>
2017-05-28 23:27:37 -0700

    add js

--- js/goodshopping.js
+++ js/goodshopping.js
@@ -3,6 +3,14 @@ var list = document.getElementById('list');
 var buttons = document.getElementById('buttons').getElementsByTagName('span');
 var prev = document.getElementById('prev');
 var next = document.getElementById('next');
+function moveleft(){
+	list.style.left = parseInt(list.style.left) - 600 + 'px'
+	}
+prev.onclick = function(){
+	list.style.left = parseInt(list.style.left) + 600 + 'px'
+	}
+	
+	alert("panda!") 
 
 
 
