e50b60325a9f9fbb30e234d0447485ca614f881c
Jack <Jackzhang700@hotmail.com>
2017-05-28 22:50:36 -0700

    action

---
+++ js/goodshopping.js
@@ -0,0 +1,12 @@
+var container = document.getElementById('container');
+var list = document.getElementById('list');
+var buttons = document.getElementById('buttons').getElementsByTagName('span');
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+
+
+
+
+
+
+// JavaScript Document
