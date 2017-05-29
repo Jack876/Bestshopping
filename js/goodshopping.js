5dbc6a252e261f22d5a053d6efbd19acce39fe0f
Jack <Jackzhang700@hotmail.com>
2017-05-29 02:02:54 -0700

    photo position test

--- js/goodshopping.js
+++ js/goodshopping.js
@@ -5,14 +5,13 @@ var prev = document.getElementById('prev');
 var next = document.getElementById('next');*/
 var list = document.getElementById('list');
 function moveleft(){
-	//list.style.left = parseInt(list.style.left) - 810 + 'px'
-	alert("panda!");
+	list.style.left = -810px;
 	}
 /*prev.onclick = function(){
 	list.style.left = parseInt(list.style.left) + 600 + 'px'
 	}*/
 	
-	// 
+	//  parseInt(list.style.left)   alert("panda!")
 
 
 
