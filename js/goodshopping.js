4d8995ed2a668b6e4a351e5c3999eaa78b67ac30
Jack <Jackzhang700@hotmail.com>
2017-05-29 22:44:26 -0700

    adjustment

--- js/goodshopping.js
+++ js/goodshopping.js
@@ -5,7 +5,9 @@ var prev = document.getElementById('prev');
 var next = document.getElementById('next');*/
 var list = document.getElementById('list');
 function moveleft(){
-	list.style.left = -810px;
+	//list.setAttribute("left","-810px");
+	list.style.left = parseInt(list.style.left)-810 +'px';
+	//list.style.left = "-810px"
 	}
 /*prev.onclick = function(){
 	list.style.left = parseInt(list.style.left) + 600 + 'px'
